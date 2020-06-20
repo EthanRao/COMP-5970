a5141a03e9b12e14126a79394e3792cafee97624
Ethan Rao <perfectraoxi@gmail.com>
2020-06-20 13:28:26 -0500

    The first iPhone app

---
+++ Assignment 1a/The first iPhone app made by Xi Rao/The first iPhone app made by Xi Rao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The first iPhone app made by Xi Rao
+//
+//  Created by Xi Rao on 5/30/20.
+//  Copyright © 2020 Xi Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assignment 1a/The first iPhone app made by Xi Rao/The first iPhone app made by Xi RaoTests/The_first_iPhone_app_made_by_Xi_RaoTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_first_iPhone_app_made_by_Xi_RaoTests.swift
+//  The first iPhone app made by Xi RaoTests
+//
+//  Created by Xi Rao on 5/30/20.
+//  Copyright © 2020 Xi Rao. All rights reserved.
+//
+
+import XCTest
+@testable import The_first_iPhone_app_made_by_Xi_Rao
+
+class The_first_iPhone_app_made_by_Xi_RaoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
