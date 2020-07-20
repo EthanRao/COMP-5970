9055b3d4f323ac1d2b77176dd42a8d953e62a5f8
Ethan Rao <perfectraoxi@gmail.com>
2020-07-20 16:21:11 -0500

    Add files via upload

---
+++ In class practice/Summer20ButtonFun/Summer20ButtonFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Summer20ButtonFun
+//
+//  Created by Xi Rao on 6/9/20.
+//  Copyright © 2020 QIang Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
